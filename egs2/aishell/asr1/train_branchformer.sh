b79281280a8f7ce15460809cc2578d6d2827584e
Swaroop Nath <swaroopnath6@gmail.com>
2023-04-27 23:38:33 +0530

    Helper file to quickly train models

---
+++ egs2/aishell/asr1/train_branchformer.sh
@@ -0,0 +1,1 @@
+./asr.sh --stage 11 --stop_stage 11 --train-set "train" --valid-set "dev" --test_sets "dev test" --asr_config "conf/train_asr_branchformer.yaml" --asr_tag "bigbird_branchformer" --nbpe 2000 --ngpu 2
